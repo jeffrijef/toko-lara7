9d9bb6cc919f40eaa8b07a65a17f3edcad91e6a8
jeffrijef <jeffriagustiar888@gmail.com>
2021-11-01 14:48:57 +0700

    menambahkan konfigurasi email,sentry dan auth

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Route::get('/debug-sentry', function () {
     throw new Exception('My first Sentry error!');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
